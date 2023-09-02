d57612d5b8099affb21092d823c8dd462cdb7163
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2023-09-02 18:28:45 -0300

    Update Avaliacao

--- src/componentes/Avaliacao/index.js
+++ src/componentes/Avaliacao/index.js
@@ -4,12 +4,18 @@ import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 
 export default function Avaliacao() {
-
+    const [valor, setValor] = React.useState(0)
     return (
         <Box sx={{
-            padding: 2}}>
+            padding: 2
+        }}>
             <Typography component="legend">Avalie seu curso</Typography>
-            <Rating name="estrelas" value={null} />
+            <Rating
+                name="estrelas"
+                value={valor}
+                onChange={(event, novoValor) => {
+                    setValor(novoValor);
+                }} />
         </Box>
     );
 }
